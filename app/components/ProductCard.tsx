984698e7da25fb5f9cda92d533338c7d83184c57
EL BOOK <80248189+ElBookyes@users.noreply.github.com>
2024-01-01 22:07:04 +0200

    Product Carousel
    Almost Done

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,15 +1,49 @@
 import React from 'react'
-import sofa from '../images/PCaroSofa.png'
+import { StaticImageData } from 'next/image'
+import AddToWish from '../icons/AddToWish'
+import StarIcon from '../icons/StarIcon'
 
-const ProductCard = () => {
+type Props = {
+  bgimg: StaticImageData
+}
+
+const ProductCard = ({ bgimg }: Props) => {
     const caroImage = {
-        backgroundImage: `url(${sofa.src})`,
+        backgroundImage: `url(${bgimg.src})`,
         backgroundPosition: 'center',
         backgroundSize: 'cover',
         backgroundRepeat: 'no-repeat'
     };
   return (
-    <div className='product-card' style={caroImage}>
+    <div className='product-card'>
+      <div className='flex | card-image' style={caroImage}>
+        <div className='flex | badge-section'>
+          <div className='badges'>
+            <div className="badge">
+              NEW
+            </div>
+            <div className='badge'>
+              -50%
+            </div>
+          </div>
+          <button className='clean'><AddToWish /></button>
+        </div>
+        <button className='card-button | kpds-button'>Add to cart</button>
+      </div>
+      <div className='card-info'>
+        <div className='flex | rating-group'>
+          <StarIcon />
+          <StarIcon />
+          <StarIcon />
+          <StarIcon />
+          <StarIcon />
+        </div>
+        <h2>Loveseat Sofa</h2>
+        <div className='flex | card-price'>
+          <p>$199.00</p>
+          <p>$400.00</p>
+        </div>
+      </div>
     </div>
   )
 }
--- app/components/ProductCarousel.tsx
+++ app/components/ProductCarousel.tsx
@@ -7,29 +7,37 @@ import 'swiper/css'
 import 'swiper/css/scrollbar';
 import { Scrollbar } from 'swiper/modules'
 import ProductCard from './ProductCard'
+import sofa from '../images/PCaroSofa.png'
+import lamp1 from '../images/lamp1.png'
+import lamp2 from '../images/lamp2.png'
+import basket from '../images/basket.png'
+import toaster from '../images/ToasterIMG.png'
+
 
 const ProductCarousel = () => {
   return (
-    <section className='kpds-section kpds-container'>
-        <div className="product-carousel-title">
-            <h1>New Arrivals</h1>
-            <Link className='flex | living-room-link' href={''}>More Products <span><ArrowRight /></span></Link>
+    <section className='product-carousel'>
+        <div className="flex | product-carousel-title">
+            <h1 className='kpds-fw-bold'>New<br />Arrivals</h1>
+            <div className='product-carousel-link'>
+              <Link className='flex | living-room-link' href={''}>More Products <span><ArrowRight /></span></Link>
+            </div>
         </div>
         <Swiper
          className='mySwiper'
          modules={[Scrollbar]}
          slidesPerView={'auto'}
-         spaceBetween={30}
+         spaceBetween={20}
          scrollbar={{ draggable: true }}
          >
-            <SwiperSlide className='product-carousel-slide'><ProductCard /></SwiperSlide>
-            <SwiperSlide className='product-carousel-slide'><ProductCard /></SwiperSlide>
-            <SwiperSlide className='product-carousel-slide'><ProductCard /></SwiperSlide>
-            <SwiperSlide className='product-carousel-slide'><ProductCard /></SwiperSlide>
-            <SwiperSlide className='product-carousel-slide'><ProductCard /></SwiperSlide>
-            <SwiperSlide className='product-carousel-slide'><ProductCard /></SwiperSlide>
-            <SwiperSlide className='product-carousel-slide'><ProductCard /></SwiperSlide>
-            <SwiperSlide className='product-carousel-slide'><ProductCard /></SwiperSlide>
+            <SwiperSlide className='product-carousel-slide'><ProductCard bgimg={sofa}/></SwiperSlide>
+            <SwiperSlide className='product-carousel-slide'><ProductCard bgimg={lamp1}/></SwiperSlide>
+            <SwiperSlide className='product-carousel-slide'><ProductCard bgimg={lamp2}/></SwiperSlide>
+            <SwiperSlide className='product-carousel-slide'><ProductCard bgimg={basket}/></SwiperSlide>
+            <SwiperSlide className='product-carousel-slide'><ProductCard bgimg={toaster}/></SwiperSlide>
+            <SwiperSlide className='product-carousel-slide'><ProductCard bgimg={sofa}/></SwiperSlide>
+            <SwiperSlide className='product-carousel-slide'><ProductCard bgimg={lamp1}/></SwiperSlide>
+            <SwiperSlide className='product-carousel-slide'><ProductCard bgimg={lamp2}/></SwiperSlide>
         </Swiper>
     </section>
   )
