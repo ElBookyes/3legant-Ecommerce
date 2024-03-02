11c9c1961ef37e7036e217e832c0f57c7f7be254
ElBookyes <dtuparov94@gmail.com>
2024-03-02 18:15:57 +0200

    fix

---
+++ app/components/valuesSection.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const valuesSection = () => {
+  return (
+    <section className='kpds-container'>
+        <div className='value-card'></div>
+    </section>
+  )
+}
+
+export default valuesSection
